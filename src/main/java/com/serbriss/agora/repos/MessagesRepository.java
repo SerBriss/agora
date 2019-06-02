4e1bd47933a70d6e4ef269c0b81c7a36631e27de
sefim <sefimenko13@gmain.com>
2019-06-03 02:37:45 +0300

    Showing list of changes. Can add and filter the messages

---
+++ src/main/java/com/serbriss/agora/repos/MessagesRepository.java
@@ -0,0 +1,11 @@
+package com.serbriss.agora.repos;
+
+import com.serbriss.agora.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessagesRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
