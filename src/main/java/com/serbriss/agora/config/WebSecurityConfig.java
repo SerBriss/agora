7481fdc509a08d696b4851e0a54fb6edfd94fd5d
sefim <sefimenko13@gmain.com>
2019-06-13 13:03:08 +0300

    adding registration

--- src/main/java/com/serbriss/agora/config/WebSecurityConfig.java
+++ src/main/java/com/serbriss/agora/config/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package com.serbriss.agora.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -28,16 +37,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll();
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("u")
-                        .password("l")
-                        .roles("USER")
-                        .build();
 
-        return new InMemoryUserDetailsManager(user);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .usersByUsernameQuery("select username, password, active from site_user where username=?")
+        .authoritiesByUsernameQuery("select u.username, ur.roles from site_user u inner join user_role ur on u.id = site_user.id where u.username=?");
     }
 }
--- src/main/java/com/serbriss/agora/controller/MainController.java
+++ src/main/java/com/serbriss/agora/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.serbriss.agora;
+package com.serbriss.agora.controller;
 
 import com.serbriss.agora.domain.Message;
 import com.serbriss.agora.repos.MessagesRepository;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessagesRepository messageRepo;
 
